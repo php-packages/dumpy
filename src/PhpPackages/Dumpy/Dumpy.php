208da20b40d62e22827bc486e1abc8cb9e7764a5
bound1ess <bound1ess.dev@gmail.com>
2015-02-20 18:54:25 +0600

    Write tests for configure() and getConfigOption().

--- src/PhpPackages/Dumpy/Dumpy.php
+++ src/PhpPackages/Dumpy/Dumpy.php
@@ -1,8 +1,44 @@
 <?php namespace PhpPackages\Dumpy;
 
+use InvalidArgumentException,
+    UnexpectedValueException;
+
 /**
  * Better var_dump for PHP.
  */
 class Dumpy
 {
+
+    /**
+     * Dumpy configuration.
+     *
+     * @var array
+     */
+    protected $config = [
+        // If a string has length of over str_max_length,
+        // the first str_max_length characters (no multibyte support as for now) will be taken,
+        // the rest (no matter how many) will be replaced with three dots (...).
+        "str_max_length" => 50,
+    ];
+
+    /**
+     * @throws \UnexpectedValueException
+     * @param string $option
+     * @return mixed
+     */
+    public function getConfigOption($option)
+    {
+        // @todo
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     * @param string $option
+     * @param mixed $value
+     * @return void
+     */
+    public function configure($option, $value)
+    {
+        // @todo
+    }
 }
--- tests/PhpPackages/Dumpy/DumpyTest.php
+++ tests/PhpPackages/Dumpy/DumpyTest.php
@@ -4,10 +4,29 @@ class DumpyTest extends \Essence\Extensions\PhpunitExtension
 {
 
     /**
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->dumpy = new Dumpy;
+    }
+
+    /**
      * @test
      */
     public function it_is_configurable()
     {
-        // @todo
+        // Let's set the configuration - change str_max_length option.
+        $this->dumpy->configure("str_max_length", 100);
+
+        // If you type something awful, Dumpy will be sad.
+        $dumpy =& $this->dumpy;
+
+        expect(function() use($dumpy) {
+            $dumpy->configure("foo", null);
+        })->toThrow("InvalidArgumentExpected");
+
+        // Let's ensure that str_max_length value was changed.
+        expect($this->dumpy->getConfigOption("str_max_length"))->toEqual(100);
     }
 }
